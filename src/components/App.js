9181c82fbdf3dd6b9458edab54048bc8c86c9c2a
faraznaeem <faraznaeem@gmail.com>
2019-10-25 06:29:47 +0200

    Adds async function to on search submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
-  onSearchSubmit(term) {
-    console.log("hello");
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID db363610ac4d1e8b8756906181f321028bdc585c375aefc334d7b0fb2f87eb35"
+      }
+    });
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
